d383275d211635660b2bd2c9c3734decf3eb8a7d
sgf <sgf.dma@gmail.com>
2022-08-11 18:00:22 +0300

    Модуль 14, задача 3.

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+
+def sum_digits(n):
+    summ = 0
+    while n > 0:
+        summ += n % 10
+        n = n // 10
+    return summ
+
+def count_digits(n):
+    count = 0
+    while n > 0:
+        count += 1
+        n = n // 10
+    return count
+
+n = int(input("Введите целое положительное число: "))
+
+if n < 0:
+    print("Число должно быть положительное.")
+else:
+    summ = sum_digits(n)
+    count = count_digits(n)
+    print("Сумма чисел:", summ)
+    print("Количество цифр в числе:", count)
+    print("Разность суммы и количества цифр:", summ - count)

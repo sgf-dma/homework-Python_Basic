84a28651714132f1638212cc07ef8ea4b51d590c
sgf <sgf.dma@gmail.com>
2022-08-12 17:22:19 +0300

    Модуль 14, задача 5.

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+
+def min_divider(n):
+    if n <= 1:
+        print("Число должно быть больше или равно 1")
+        return 0
+    for d in range(2, n + 1):
+        if n % d == 0:
+            return d
+
+n = int(input("Введите число: "))
+print("наименьший делитель, отличный от единицы:", min_divider(n))

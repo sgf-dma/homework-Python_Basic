73b731ce2b21cfec8f6f1e01aec6492b27294be3
sgf <sgf.dma@gmail.com>
2022-08-12 17:50:03 +0300

    Модуль 14, задача 6.

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+
+def in_circle(r, x, y):
+    return x*x + y*y <= r*r
+
+print("Введите координаты монетки:")
+x = float(input("X: "))
+y = float(input("Y: "))
+r = float(input("Введите радиус: "))
+
+if in_circle(r, x, y):
+    print("Монетка где-то рядом")
+else:
+    print("Монетки в области нет")

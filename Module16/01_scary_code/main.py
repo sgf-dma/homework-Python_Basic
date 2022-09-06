b6b823a3cc75fbb394811f688c17b320836b0849
sgf <sgf.dma@gmail.com>
2022-09-06 17:06:59 +0300

    Задача 1.

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,18 @@
+
+def elem(y, list):
+    for x in list:
+        if x == y:
+            return True
+    return False
+
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+a.extend(b)
+print(a.count(5))
+while elem(5, a):
+    a.remove(5)
+a.extend(c)
+print(a.count(3))
+print(a)
 
-# TODO переписать программу

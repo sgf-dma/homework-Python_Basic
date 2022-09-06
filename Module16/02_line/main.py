94a3ba5b1425bad49e78c553a476e7eab5f3309a
sgf <sgf.dma@gmail.com>
2022-09-06 18:24:07 +0300

    Задача 2.

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+
+def join_lists(xs, ys):
+    i = 0
+    j = 0
+    z = []
+    while i < len(xs) and j < len(ys):
+        if xs[i] <= ys[j]:
+            z.append(xs[i])
+            i += 1
+        else:
+            z.append(ys[j])
+            j += 1
+    if i == len(xs):
+        z.extend(ys[j:])
+    else:
+        z.extend(xs[i:])
+    return z
+
+height = 160
+a = list(range(160, 177, 2))
+b = list(range(162, 181, 3))
+
+print("Отсортированный список учеников:", join_lists(a, b))

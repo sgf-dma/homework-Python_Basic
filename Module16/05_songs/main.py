1e2a377881354e2ac2be870e6615683ef46a01ae
sgf <sgf.dma@gmail.com>
2022-09-10 15:30:24 +0300

    Задача 5.

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -10,4 +10,18 @@ violator_songs = [
     ['Clean', 5.83]
 ]
 
-# TODO здесь писать код
+i_name = 0
+i_duration = 1
+
+cnt = int(input("Сколько песен выбрать? "))
+total_duration = 0
+for i in range(cnt):
+    song = input("Название " + str(i+1) + "-й песни: ")
+    for v in violator_songs:
+        if v[i_name] == song:
+            total_duration += v[i_duration]
+            break
+    else:
+        print("Нету такой песни.")
+
+print("Общее время звучания песен:", round(total_duration, 2), "минуты")

bd5d6c42bc8883baab79bf4c997e2204a6278530
sgf <sgf.dma@gmail.com>
2022-09-10 15:07:46 +0300

    Задача 4.

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,38 @@
+
+def elem(y, xs):
+    for x in xs:
+        if x == y:
+            return True
+    return False
+
+def work(reply):
+    if reply == "пришёл":
+        guest = input("Имя гостя: ")
+        if len(guests) >= 6:
+            print("Прости, ", guest, ", но мест нет.", sep='')
+            return True
+        guests.append(guest)
+        print("Привет,", guest)
+    elif reply == "ушёл":
+        guest = input("Имя гостя: ")
+        if not elem(guest, guests):
+            print("Такого гостя не было.")
+            return True
+        guests.remove(guest)
+        print("Пока,", guest)
+    elif reply == "Пора спать":
+        print("Вечеринка закончилась, все легли спать.")
+        return False
+    else:
+        print("Не понимаю о чём речь..")
+    return True
+
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+cont = True
+while cont == True:
+    print("Сейчас на вечеринке", len(guests), "человек:", guests)
+    reply = input("Гость пришёл или ушёл? ")
+    cont = work(reply)
+    print()
+

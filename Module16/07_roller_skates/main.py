d2641dda94c4a44db123f79c10b4d57f84180e5a
sgf <sgf.dma@gmail.com>
2022-09-12 15:26:21 +0300

    Задача 7.

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+n_skates = int(input("Кол-во коньков: "))
+skates = []
+for i in range(n_skates):
+    x = int(input("Размер " + str(i+1) + "-й пары: "))
+    skates.append(x)
+
+n_foots = int(input("Кол-во людей: "))
+foots = []
+for i in range(n_foots):
+    x = int(input("Размер ноги " + str(i+1) + "-го человека: "))
+    foots.append(x)
+
+skates.sort()
+n_people = 0
+for foot in foots:
+    for skate in skates:
+        if foot <= skate:
+            skates.remove(skate)
+            n_people += 1
+            break
+
+print("Наибольшее кол-во людей, которые могут взять ролики:", n_people)

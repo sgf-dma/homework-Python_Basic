0bb6bcf9991b6fcac546accbb505fc814ccff2da
sgf <sgf.dma@gmail.com>
2022-10-18 23:40:21 +0300

    Модуль 17. Задача 1.

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+
+def elem(c, list):
+    return len([x for x in list if x == c]) > 0
+
+text = input("Введите текст: ")
+vowels = "аеёиыоуэюя"
+
+text_vowels = [c for c in text if elem(c, vowels)]
+print("Список гласных букв:", text_vowels)
+print("Длина списка:", len(text_vowels))

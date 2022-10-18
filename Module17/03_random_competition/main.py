1ca96ce0efb71bee91d93e9480fa4a911f32329f
sgf <sgf.dma@gmail.com>
2022-10-19 00:04:38 +0300

    Модуль 17. Задача 3.

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import random
+
+def gen_team():
+    return [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+team_a = gen_team()
+team_b = gen_team()
+
+match = [max(team_a[i], team_b[i]) for i in range(len(team_a))]
+print(team_a)
+print(team_b)
+print(match)

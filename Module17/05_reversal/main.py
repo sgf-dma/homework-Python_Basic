39cd85c268378d69957661d79fbbe76dd6c7a7dd
sgf <sgf.dma@gmail.com>
2022-10-19 20:50:02 +0300

    Модуль 17. Задача 5.

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+list = input("Введите строку: ")
+
+h_ixs = [i for i, c in enumerate(list) if c == 'h']
+h_between = list[h_ixs[-1]-1:h_ixs[0]:-1]
+print("Развёрнутая последовательность между первым и последним h:", h_between)

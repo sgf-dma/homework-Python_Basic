819d8b3f68764634792766470e2f4d01fd969aa5
sgf <sgf.dma@gmail.com>
2022-10-20 17:17:09 +0300

    Модуль 17. Задача 9.

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+result = [x for lst_2 in nice_list for lst in lst_2 for x in lst]
+print(result)

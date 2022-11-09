fac8efcd8e732d2524d0b0ef4198525db2a4afd0
sgf <sgf.dma@gmail.com>
2022-11-09 21:57:26 +0300

    Модуль 18, задача 2.

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+
+text = input("Введите строку: ").split()
+longest_word = max(text, key=len)
+print(f"Самое длинное слово: {longest_word}")
+print(f"Длина этого слова: {len(longest_word)}")

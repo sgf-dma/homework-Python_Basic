e607ae81bd8ecf1dc141faeedc283cc6bbf08bcb
sgf <sgf.dma@gmail.com>
2022-11-10 22:28:56 +0300

    Модуль 18, задача 6.

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+text = input("Введите строку: ")
+
+zipped = []
+if text:
+    prev = text[0]
+    n = 0
+    for c in text:
+        if c == prev:
+            n += 1
+        else:
+            zipped.append(''.join([prev, str(n)]))
+            n = 1
+            prev = c
+    zipped.append(''.join([prev, str(n)]))
+print(''.join(zipped))

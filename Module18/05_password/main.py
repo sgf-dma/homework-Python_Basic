9de6ccf47abdc66fb2f6f9f799ccc9d9598e399f
sgf <sgf.dma@gmail.com>
2022-11-10 21:54:18 +0300

    Модуль 18, задача 5.

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+while True:
+    pw = input("Придумайте пароль: ")
+    if (
+        len(pw) > 7
+        and len([c for c in pw if c.isupper()]) > 0
+        and len([c for c in pw if c.isdigit()]) > 2
+    ):
+        print("Это надёжный пароль!")
+        break
+    else:
+        print("Пароль ненадёжный. Попробуйте ещё раз.")

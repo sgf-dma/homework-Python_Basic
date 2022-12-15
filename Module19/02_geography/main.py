d192fbf50663bcfaea75458c0380f3c39ca89a82
sgf <sgf.dma@gmail.com>
2022-12-15 20:50:02 +0300

    Модуль 19. Задача 2.

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+n_countries = int(input("Количество стран: "))
+towns = dict()
+for i in range(n_countries):
+    l = input(f"{i+1}-ая страна: ").split()
+    country = l[0]
+    for t in l[1:]:
+        towns[t] = country
+
+for greet in ["Первый", "Второй", "Третий"]:
+    t = input(f"{greet} город: ")
+    if t in towns:
+        print(f"Город {t} расположен в стране {towns[t]}")
+    else:
+        print(f"По городу {t} данных нет.")

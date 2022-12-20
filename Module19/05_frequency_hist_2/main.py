8e651d16c1671b86252026ef9a64f1acbc54a32c
sgf <sgf.dma@gmail.com>
2022-12-20 16:42:56 +0300

    Модуль 19. Задача 5.

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+
+def show_dict(d):
+    return "\n".join([f"{k}: {v}" for k, v in d.items()])
+
+text = input("Введите текст: ")
+freq = dict()
+for c in text:
+    freq[c] = freq.get(c, 0) + 1
+
+char_by_freq = dict()
+for c, fr in freq.items():
+    if fr not in char_by_freq:
+        char_by_freq[fr] = []
+    char_by_freq[fr].append(c)
+
+print("Оригинальный словарь частот:\n", show_dict(freq), sep='')
+print("Инвертированный словарь частот:\n", show_dict(char_by_freq), sep='')

3503f7c825ed5f1334128dd8f59c0571e67b43da
sgf <sgf.dma@gmail.com>
2022-12-23 19:59:08 +0300

    Модуль 19. Задача 6.

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+vocabulary = dict()
+n = int(input("Введите количество пар слов: "))
+for i in range(0, n):
+    w = input(f"{i+1}-ая пара: ").split()
+    if len(w) < 3:
+        print("Некорректная пара")
+        continue
+    vocabulary[w[0].lower()] = w[2].lower()
+    vocabulary[w[2].lower()] = w[0].lower()
+
+word = input("Введите слово: ")
+while word != "":
+    syn = vocabulary.get(word.lower(), None)
+    if not syn:
+        print("Такого слова в словаре нет.")
+    else:
+        print("Синоним:", syn)
+    word = input("Введите слово: ")

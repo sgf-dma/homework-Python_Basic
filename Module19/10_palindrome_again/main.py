d30187bc1a8dfe279262454534bcb7328c2aedce
sgf <sgf.dma@gmail.com>
2022-12-23 19:57:05 +0300

    Модуль 19. Задача 10.

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+inp = input("Введите строку: ")
+char_count = dict()
+for c in inp:
+    char_count[c] = char_count.get(c, 0) + 1
+
+odd_chars = [x for x in char_count.values() if x % 2 != 0]
+if len(odd_chars) < 2:
+    print("Можно сделать палиндромом")
+else:
+    print("Нельзя сделать палиндромом")
